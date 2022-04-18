de09d2cf31484fef9386dd8c49092f14bb3c3db7
JayeshRocks <78260688+JayeshRocks@users.noreply.github.com>
2022-04-18 13:23:27 +0530

    Update rproxy.sh

--- rproxy.sh
+++ rproxy.sh
@@ -39,21 +39,21 @@ sudo apt-get update
 sudo apt-get -y install certbot python3-certbot-nginx
 
 echo ""
-echo "--------------------------"
+echo "-----------------------"
 echo ""
-echo "Awaiting your command..."
+echo "Awaiting your signal..."
 echo ""
-echo "--------------------------"
+echo "-----------------------"
 echo ""
 echo ""
 echo "Please follow the mandatory steps below:"
 echo ""
 echo "Navigate to CloudFlare.com and login"
-echo "Choose the domain you want to have your map on"
+echo "Choose the domain you want to have your website on"
 echo "Select “DNS” from the menu at the top of the screen"
 echo "Click “Add Record”"
 echo "Set “Type” to “A”"
-echo "Set “Name” to whatever you want your subdomain to be (Eg: map)"
+echo "Set “Name” to whatever you want your subdomain to be (Eg: www)"
 echo "Set “IPv4 address” to the IP Address of your Machine"
 echo "Set Proxy Status to "DNS Only""
 echo "Click "Save""
@@ -74,10 +74,10 @@ echo ""
 
 sleep 3
 
-echo "What is the (sub)domain name you want to use for Dynmap? (Eg. maps.yoursite.com)"                                                                                                                   
+echo "What is the (sub)domain name you want to use for website? (Eg. www.yoursite.com)"                                                                                                                   
 read ccdomain                                                                                                                                                     
                                                                                                                                                                   
-echo "What is the IP address of your Minecraft server and the port Dynmap is running on (Eg. 192.168.1.101:8192)"                                                                                   
+echo "What is the IP address and the port of your website which it's running on (Eg. 192.168.1.101:1024)"                                                                                   
 read ccip                                                                                                                                                         
                                                                                                                                                                   
 echo " server {                                                                                                                                                   
@@ -97,4 +97,4 @@ certbot --nginx
   
 sudo service nginx reload
                                                                                                                                                                 
-echo "Your Dynmap reverse proxy should now be ready and available at https://$ccdomain" 
+echo "Your reverse proxy should now be ready and available at https://$ccdomain" 
